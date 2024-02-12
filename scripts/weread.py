89e3d93c6e9dcfb3b60c5c51ed09bacf29a4a1c3
Arron <46951903+lx111lx@users.noreply.github.com>
2024-02-12 23:02:49 +0800

    Update weread.py

--- scripts/weread.py
+++ scripts/weread.py
@@ -19,23 +19,45 @@ from utils import (
 def get_bookmark_list(page_id, bookId):
     """获取我的划线"""
     filter = {"property": "Books", "relation": {"contains": page_id}}
+
+    """notion_helper.query_all_by_book函数传入了两个参数，一个是查询到的数据库id，另一个是筛选条件"""
     results = notion_helper.query_all_by_book(
         notion_helper.bookmark_database_id, filter
     )
+    """
+    从上面筛选到的结果中，以富文本的形式提取的"blockId"循环赋值给 同样提取到的"bookmarkId"，并将每次循环的键和值构建成一个临时字典
+    """
     dict1 = {
         get_rich_text_from_result(x, "bookmarkId"): get_rich_text_from_result(
             x, "blockId"
         )
         for x in results
     }
+    """
+    将通过notion API获取到的"id"循环赋值给上面以富文本的形式提取的"blockId"，并将每次循环的键和值构建成一个临时字典
+    """
     dict2 = {get_rich_text_from_result(x, "blockId"): x.get("id") for x in results}
     bookmarks = weread_api.get_bookmark_list(bookId)
     for i in bookmarks:
+        """
+        如果notion API获取到的"bookmarkId"键存在临时字典dict1的键中，则从dict1中移除这个对应的键值对，
+        并且把这个键对值添加到临时的"i"字典中，其中键是"blockId"，值是从dict1中移除的值。
+        """
         if i.get("bookmarkId") in dict1:
             i["blockId"] = dict1.pop(i.get("bookmarkId"))
+    # 到这一步，dict1中剩余的都是Notion API没有获取到的blockID，接下来的循环就是把这些dict1里剩余的都从Notion里删除
+    """ 原始代码
     for blockId in dict1.values():
         notion_helper.delete_block(blockId)
         notion_helper.delete_block(dict2.get(blockId))
+    """
+    # 我修改的，加了条件判断：在dict1中已经删除的，dict2不会再次删除
+    for blockId in dict1.values():
+        notion_helper.delete_block(blockId)
+        # 检查dict2中的blockId是否已经在dict1的删除操作中被处理
+        correspondingBlockId = dict2.get(blockId)
+        if correspondingBlockId and correspondingBlockId not in dict1.values():
+            notion_helper.delete_block(correspondingBlockId)
     return bookmarks
 
 
@@ -54,9 +76,18 @@ def get_review_list(page_id,bookId):
     for i in reviews:
         if i.get("reviewId") in dict1:
             i["blockId"] = dict1.pop(i.get("reviewId"))
+    """ 原始代码 
     for blockId in dict1.values():
         notion_helper.delete_block(blockId)
         notion_helper.delete_block(dict2.get(blockId))
+    """
+    # 我修改的，加了条件判断：在dict1中已经删除的，dict2不会再次删除
+    for blockId in dict1.values():
+        notion_helper.delete_block(blockId)
+        # 检查dict2中的blockId是否已经在dict1的删除操作中被处理
+        correspondingBlockId = dict2.get(blockId)
+        if correspondingBlockId and correspondingBlockId not in dict1.values():
+            notion_helper.delete_block(correspondingBlockId)
     return reviews
 
 
@@ -69,106 +100,7 @@ def check(bookId):
     if len(response["results"]) > 0:
         return response["results"][0]["id"]
     return None
-"""
-def update_callout_style(content, style, colorStyle, reviewId):
-"""检查插入的callout样式是否与目标样式一致"""
-    callout_color = None
-    if color == "red_background" and colorStyle == 1:
-        pass
-    elif color == "purple_background" and colorStyle == 2:
-        pass
-    elif color == "blue_background" and colorStyle == 3:
-        pass
-    elif color == "green_background" and colorStyle == 4:
-        pass
-    elif color == "yellow_background" and colorStyle == 5:
-        pass
-    else:
-        callout_color = color
-    
-    callout_icon = None
-    if style == 0:
-        if colorStyle == 1 and icon == get_icon(STRAIGHTLINE_RED_ICON_URL):
-            pass
-        elif colorStyle == 2 and icon == get_icon(STRAIGHTLINE_PURPLE_ICON_URL):
-            pass
-        elif colorStyle == 3 and icon == get_icon(STRAIGHTLINE_BLUE_ICON_URL):
-            pass
-        elif colorStyle == 4 and icon == get_icon(STRAIGHTLINE_GREEN_ICON_URL):
-            pass
-        elif colorStyle == 5 and icon == get_icon(STRAIGHTLINE_YELLOW_ICON_URL):
-            pass
-        elif colorStyle == None and icon == get_icon(FILLING_BROWN_ICON_URL):
-            pass
-        else:
-            callout_icon = icon
 
-     elif style == 1:
-        if colorStyle == 1 and icon == get_icon(FILLING_RED_ICON_URL):
-            pass
-        elif colorStyle == 2 and icon == get_icon(FILLING_PURPLE_ICON_URL):
-            pass
-        elif colorStyle == 3 and icon == get_icon(FILLING_BLUE_ICON_URL):
-            pass
-        elif colorStyle == 4 and icon == get_icon(FILLING_GREEN_ICON_URL):
-            pass
-        elif colorStyle == 5 and icon == get_icon(FILLING_YELLOW_ICON_URL):
-            pass
-        elif colorStyle == None and icon == get_icon(FILLING_BROWN_ICON_URL):
-            pass
-        else:
-            callout_icon = icon
-
-      elif style == 2:
-        if colorStyle == 1 and icon == get_icon(WAVELINE_RED_ICON_URL):
-            pass
-        elif colorStyle == 2 and icon == get_icon(WAVELINE_PURPLE_ICON_URL):
-            pass
-        elif colorStyle == 3 and icon == get_icon(WAVELINE_BLUE_ICON_URL):
-            pass
-        elif colorStyle == 4 and icon == get_icon(WAVELINE_GREEN_ICON_URL):
-            pass
-        elif colorStyle == 5 and icon == get_icon(WAVELINE_YELLOW_ICON_URL):
-            pass
-        elif colorStyle == None and icon == get_icon(WAVELINE_BROWN_ICON_URL):
-            pass
-        else:
-            callout_icon = icon
-
-    if colorStyle == 1 and icon = get_icon(NOTE_RED_ICON_URL):
-        pass
-    elif colorStyle == 2 and icon == get_icon(NOTE_PURPLE_ICON_URL):
-        pass
-    elif colorStyle == 3 and icon == get_icon(NOTE_BLUE_ICON_URL):
-        pass
-    elif colorStyle == 4 and icon == get_icon(NOTE_GREEN_ICON_URL):
-        pass
-    elif colorStyle == 5 and icon == get_icon(NOTE_YELLOW_ICON_URL):
-        pass
-    elif colorStyle == None and icon == get_icon(NOTE_BROWN_ICON_URL):
-        pass
-    else:
-        callout_icon = icon
-
-    if callout_icon is not None or callout_color is not None:
-        return {
-            "type": "callout",
-            "callout": {
-                "rich_text": [
-                    {
-                        "type": "text",
-                        "text": {
-                            "content": content[:MAX_LENGTH],
-                        },
-                    }
-                ],
-                "icon": callout_icon,
-                "color": callout_color,
-            },
-        }
-    else:
-        pass
-"""
 
 def get_sort():
     """获取database中的最新时间"""
@@ -262,15 +194,18 @@ def sort_notes(page_id, chapter, bookmark_list):
 
 def append_blocks(id, contents):
     print(f"笔记数{len(contents)}")
+    #插入目录？
     before_block_id = ""
     block_children = notion_helper.get_block_children(id)
     if len(block_children) > 0 and block_children[0].get("type") == "table_of_contents":
         before_block_id = block_children[0].get("id")
     else:
         response = notion_helper.append_blocks(
-            block_id=id, children=[get_table_of_contents()]
+            block_id = id, children = [get_table_of_contents()]
         )
         before_block_id = response.get("results")[0].get("id")
+
+    #插入callout block内容？
     blocks = []
     sub_contents = []
     l = []
@@ -297,6 +232,7 @@ def append_blocks(id, contents):
 
     if len(blocks) > 0:
         l.extend(append_blocks_to_notion(id, blocks, before_block_id, sub_contents))
+
     for index, value in enumerate(l):
         print(f"正在插入第{index+1}条笔记，共{len(l)}条")
         if "bookmarkId" in value:
@@ -306,7 +242,7 @@ def append_blocks(id, contents):
         else:
             notion_helper.insert_chapter(id, value)
 
-
+# 把获取到的划线内容，转成callout block
 def content_to_block(content):
     if "bookmarkId" in content:
         return get_callout(
@@ -314,7 +250,6 @@ def content_to_block(content):
             content.get("style"),
             content.get("colorStyle"),
             content.get("reviewId"),
-           #我加的 content.get("icon"),
         )
     elif "reviewId" in content:
         return get_callout(
@@ -322,18 +257,18 @@ def content_to_block(content):
             content.get("style"),
             content.get("colorStyle"),
             content.get("reviewId"),
-            #我加的content.get("icon"),
         )
     else:
         return get_heading(content.get("level"), content.get("title"))
 
-
+# 把转换好的callout block，添加到notion
 def append_blocks_to_notion(id, blocks, after, contents):
     response = notion_helper.append_blocks_after(
         block_id=id, children=blocks, after=after
     )
     results = response.get("results")
     l = []
+    #检查是否有摘要（笔记？）
     for index, content in enumerate(contents):
         result = results[index]
         if content.get("abstract") != None and content.get("abstract") != "":
